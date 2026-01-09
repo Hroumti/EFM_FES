178f7831af9ce729735a6bcd68d445f2bdde016c
Omar Hroumti <hroumtiomar11@gmail.com>
2026-01-09 10:02:29 +0100

    recommit

---
+++ src/features/studentSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    studentList: []
+}
+
+const StudentSlice = createSlice({
+    name: "Students",
+    initialState,
+    reducers: {
+        addStudent: (state, action) => {
+            state.studentList.push({ id: Date.now(), ...action.payload })
+        },
+        editStudent: (state, action) => {
+            const index = state.studentList.findIndex(s => s.id === action.payload.id)
+            if (index !== -1) {
+                state.studentList[index] = action.payload
+            }
+        },
+        removeStudent: (state, action) => {
+            state.studentList = state.studentList.filter(s => s.id !== action.payload.id)
+        },
+        
+    }
+})
+
+export const { addStudent, editStudent, removeStudent, filterBySearchTerm } = StudentSlice.actions
+export default StudentSlice.reducer
